fca4d3e0299630bf1c0163e70ffd1d9df1b45111
sinchang <sinchangwen@gmail.com>
2019-11-20 22:47:15 +0800

    Encode path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # functions
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/348538be-0963-4121-9838-8743940742b1/deploy-status)](https://app.netlify.com/sites/sfuns/deploys)
--- functions/contact.js
+++ functions/contact.js
@@ -12,7 +12,7 @@ exports.handler = (event, context, callback) => {
   }
 
   axios(
-    `https://api.telegram.org/bot${process.env.BOT_API_KEY}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${text}`
+    `https://api.telegram.org/bot${process.env.BOT_API_KEY}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${encodeURIComponent(text)}`
   )
     .then(() => {
       callback(null, {
@@ -22,7 +22,7 @@ exports.handler = (event, context, callback) => {
     })
     .catch(e => {
       callback(null, {
-        statusCode: 200,
+        statusCode: 500,
         body: JSON.stringify({ message: e.message })
       })
     })
